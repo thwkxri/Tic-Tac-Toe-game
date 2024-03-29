456ebf37c9bd20089fb2c615e75b87bba7b93531
thwkxri <153106847+thwkxri@users.noreply.github.com>
2024-03-29 16:24:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let currentPlayer = "X"; 
+let cells = document.querySelectorAll(".cell");
+
+let winInfo = document.getElementById('win-info');
+
+const restartBtn = document.getElementById('restart-btn');
+
+function cellClick(cell) {
+    if (!cell.textContent) {
+        cell.textContent = currentPlayer;
+        if (checkWin()) {
+            winInfo.innerHTML = `${currentPlayer} выиграли!`;
+            
+        } else {
+            currentPlayer = currentPlayer == "X" ? "O" : "X";
+        }
+    }
+}
+
+function checkWin() {
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    return winningCombos.some(combo => {
+        return cells[combo[0]].textContent &&
+            cells[combo[0]].textContent === cells[combo[1]].textContent &&
+            cells[combo[1]].textContent === cells[combo[2]].textContent;
+    });
+}
+
+function init() {
+    cells.forEach(cell => {
+        cell.addEventListener('click', (e) => cellClick(cell));
+    });
+}
+
+init();
+
+restartBtn.addEventListener('click', (e) => {
+  cells.forEach(cell => {
+    cell.textContent = '';
+  })
+  winInfo.textContent = '';
+})
+
